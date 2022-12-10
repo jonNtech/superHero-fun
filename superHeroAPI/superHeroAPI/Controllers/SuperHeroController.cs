7d9575de9a0ad8f3f69e18f2bc0fef6f2c409e95
Jon Pena <iamjonntech@gmail.com>
2022-12-09 20:41:31 -0700

    first commit with basic setup

---
+++ superHeroAPI/superHeroAPI/Controllers/SuperHeroController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace superHeroAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetSuperHeroes ()
+        {
+            return new List <SuperHero>
+            {
+                new SuperHero
+                {
+                    Id = 1,
+                    Name = "SpiderMan",
+                    FirstName = "Peter",
+                    LastName = "Parker",
+                    Place = "New York City"
+                }
+            };
+        }
+    }
+}
